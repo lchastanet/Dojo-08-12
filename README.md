3a36d60c4acd0618ab082e3e5a66623c37ed9aed
Loris <45939615+lchastanet@users.noreply.github.com>
2022-12-08 10:34:27 +0100

    init files

---
+++ README.md
@@ -0,0 +1,15 @@
+# Second Dojo WCS
+
+First you need to install dev dependencies :
+
+```shell
+npm i
+```
+
+then to test your code :
+
+```shell
+npm run test:[name of the kata]
+```
+
+Tips : to work properly, your function must have the same name as the export.
---
+++ soccer/soccer.test.js
@@ -0,0 +1,12 @@
+const assert = require("assert");
+const getScore = require("./soccer.js");
+
+assert.strictEqual(typeof getScore, "function");
+assert.strictEqual(getScore.length, 1);
+assert.deepStrictEqual(getScore(null), "Illegal argument");
+assert.strictEqual(getScore([]), "0-0 : draw");
+assert.strictEqual(getScore([1, 1, 1]), "3-0 : team 1 wins the game");
+assert.strictEqual(getScore([2, 2, 2]), "0-3 : team 2 wins the game");
+assert.strictEqual(getScore([1, 1, 2, 2]), "2-2 : draw");
+assert.strictEqual(getScore([1, 2, 1, 3, 1, 2]), "Illegal argument");
+assert.strictEqual(getScore(["1", "2"]), "Illegal argument");
